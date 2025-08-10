b3174186abeaacd5585b3e3eee9de64b39b7f9a9
tyujjwal6 <ujjwal.singh.csit.2022@miet.ac.in>
2025-08-10 17:00:55 +0530

    first commit

--- src/Pages/Hero.jsx
+++ src/Pages/Hero.jsx
@@ -80,7 +80,7 @@ const Hero = () => {
         */}
         <div 
           className="hero-bg-img absolute inset-0 w-full h-full bg-cover bg-center"
-          style={{ backgroundImage: `url('../assets/hero.jpg')` }}
+          style={{ backgroundImage: `url('./src/assets/Hero.jpg')` }}
         ></div>
 
         <div className="hero-overlay bg-opacity-30"></div>
